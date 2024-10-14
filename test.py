24bd5dba9eafae3133953afa19fd8a78219a88c3
valery2024-2024 <sof20b13@gmail.com>
2024-10-14 11:11:49 +0300

    add lab 1

--- test.py
+++ test.py
@@ -1,2 +1,48 @@
 i =1 
 print(i)
+int=10
+float=3.14
+str="Hello Valera"
+bool=True
+
+print("int =")
+print("float =")
+print("str =")
+print("bool")
+
+print("Тип int:", type (int))
+print("Тип float:", type(float))
+print("Тип str:", type(str))
+print("Тип bool:", type(bool))
+
+a=5+5
+b=5-4
+c=5*3
+d=6/2
+f=2^2
+print("+")
+print("-")
+print("*")
+print("/")
+print("^")
+
+a=4
+b=2
+c=pow(a,b)
+print(result)
+round(2)
+abs(2>3)
+modul=2%2
+print("%")
+a=float(input("2"))
+b=float(input("4"))
+c=float(input("3"))
+average=a+b+c/3
+print("f середнє арифметичне з чисел {a}, {b}, {c}, {average}")
+
+name = "Валерій"
+fge = 43
+gretinng = "Привіт моє ім'я" + name + "."
+format("ім'я")
+
+
